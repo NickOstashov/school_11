c7edd75fbf92fdb83ec29efc416655dff2c98d4c
NickOstashov <111501728+NickOstashov@users.noreply.github.com>
2025-02-22 11:40:13 +0500

    add new class

---
+++ class/car.py
@@ -0,0 +1,25 @@
+class Car:
+    def __init__(self, car_color, door_number, fuel):
+        self.color : str = car_color
+        self.doors_number : int = door_number
+        self.fuel : str = fuel
+
+    def print_info(self):
+        print("цвет :", self.color)
+        print("количество дверей :", self.doors_number)
+        print("топливо: ", self.fuel)
+
+
+def main():
+    car1 = Car("желтая", 2, "бензин")
+    #car1.print_info()
+
+    car2 = Car("Черный", 4, "электричество")
+    car2.print_info()
+
+
+
+if __name__ == "__main__":
+    main()
+
+
